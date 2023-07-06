9b84050f04a1d5014c4c9fe8715f60024e66b597
Yuuunna <y.sato.23.1.762@4nonome.com>
2023-07-06 22:09:42 +0900

    制御構文課題

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,22 @@
 a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
-# TODO
+if int(a) <= 1:
+    print("False")
+else:
+    for i in range(2, int(int(a) ** 0.5) + 1):
+        if int(a) % i == 0:
+            print(a + "は素数でない")
+            break
+    else:
+        print(a + "は素数である")
+
+if int(b) <= 1:
+    print("False")
+else:
+    for i in range(2, int(int(b) ** 0.5) + 1):
+        if int(b) % i == 0:
+            print(b + "は素数でない")
+            break
+    else:
+        print(b + "は素数である")
